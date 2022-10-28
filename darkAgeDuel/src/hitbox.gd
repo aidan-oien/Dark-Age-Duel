9974098c97790ab88c44d8bf84e49c08c8d53d8c
Aidan <oien313@gmail.com>
2022-10-28 16:51:34 -0400

    slight changes to level and hitbox script

--- darkAgeDuel/src/hitbox.gd
+++ darkAgeDuel/src/hitbox.gd
@@ -1,15 +1,11 @@
 extends CollisionShape2D
 
 export var is_directional: = true	# sets if you want a direction, if not, `degree`, `constLaunch`, and `launch` will be ignored
-export var degree: = 135			# degree of launch if is directional is true
-export var const_launch: = true		# sets a constant launch power not based off of percentage
+export var degree: = 135			# degree of launch
+export var const_launch: = true		# sets a constant launch power not based off of percentage, if not, `launch` will be ignored
 export var launch: = -300			# how far you want to lanch if `constLaunch` is true
-export var percentDelt: = 5.0		# how much percent you'd like an attack to give
+export var percentDelt: = 5.0		# how much percent you'd like an attack to deal
 
-func _ready() -> void:
+func _physics_process(delta: float) -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	
